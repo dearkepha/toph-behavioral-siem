86456800db1f5f73962b28ee6c134e0d8768c378
Pedro <cabralpinheiro4@gmail.com>
2026-06-13 15:46:17 -0300

    improvements on toph.py logic

--- toph.py
+++ toph.py
@@ -1,7 +1,5 @@
 import json
-from collections import Counter
 suspicious_ips = []
-cnt = Counter()
 
 def read_entry():
     with open("log.json", "r") as log:
@@ -13,16 +11,20 @@ def read_entry():
             username = clear_entry["username"]
             login = clear_entry["login"]
         
-            if ip.startswith("192.168.1.") or ip.startswith("10.0."):
+            if ip.startswith("192.168.1.") or ip.startswith("10.0."): #every ip that is not from an expected network get's dumped into a list to be analyzed
                 continue
             suspicious_ips.append((ip, username, login))
 
 def burst_detector():
     read_entry()
+    ip_data = {}
+    
     for ip, username, login in suspicious_ips:
-        cnt[ip] += 1
-
-    for ip in cnt:
-        print(f"ip {ip} tried to log in a total of {cnt[ip]} times under the username {username}. {login} login attempt. It got in after {cnt[ip]-1} tries")
+        if ip not in ip_data:
+            ip_data[ip] = {"count" : 0, "username" : username, "login" : login} #ip_data is a dictionary where every value for that ip is another dictionary containing info about it's respective address
+        ip_data[ip]["count"]+=1
+        ip_data[ip]["login"] = login
+    for ip in ip_data:
+        print(f"ip {ip} tried to log in a total of {ip_data[ip]['count']} times under the username {ip_data[ip]['username']}. Login {ip_data[ip]['login']}")
 burst_detector()
 
